8522f610219ff52f7a8af4b2bc28391bce31a1a8
ProgerUser <said.pachuliya@mail.ru>
2021-10-27 22:50:53 +0300

    ++

--- MinistryofJustice/src/ru/psv/mj/prjmngm/inboxdocs/EditPmDocC.java
+++ MinistryofJustice/src/ru/psv/mj/prjmngm/inboxdocs/EditPmDocC.java
@@ -1,20 +1,12 @@
 package ru.psv.mj.prjmngm.inboxdocs;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Types;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 
-import javafx.application.Platform;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -25,13 +17,9 @@ import javafx.stage.WindowEvent;
 import javafx.util.StringConverter;
 import ru.psv.mj.admin.users.USR;
 import ru.psv.mj.app.main.Main;
-import ru.psv.mj.msg.Msg;
 import ru.psv.mj.util.ConvConst;
 import ru.psv.mj.utils.DbUtil;
-import ru.psv.mj.widgets.FxUtilTest;
 import ru.psv.mj.widgets.MaskField;
-import ru.psv.mj.www.plushaze.traynotification.Notifications;
-import ru.psv.mj.www.plushaze.traynotification.TrayNotification;
 
 public class EditPmDocC {
 	/**
@@ -87,43 +75,7 @@ public class EditPmDocC {
 
 	void SaveCompare() {
 		try {
-			CallableStatement callStmt = conn.prepareCall("{ call PM_EMP_PKG.EDIT(?,?,?,?,?,?,?,?,?,?,?)}");
-			// Ошибка
-			callStmt.registerOutParameter(1, Types.VARCHAR);
-			// ИД сотрудника
-			if (pm_emp != null) {
-				callStmt.setLong(2, pm_emp.getEMP_ID());
-			} else {
-				callStmt.setNull(2, java.sql.Types.INTEGER);
-			}
-			/* Фамилия */
-			callStmt.setString(3, EMP_LASTNAME.getText());
-			/* Имя */
-			callStmt.setString(4, EMP_FIRSTNAME.getText());
-			/* Отчество */
-			callStmt.setString(5, EMP_MIDDLENAME.getText());
-			/* Должность */
-			callStmt.setString(6, EMP_POSITION.getText());
-			/* Email */
-			callStmt.setString(7, EMP_EMAIL.getText());
-			/* Телефон */
-			callStmt.setString(8, EMP_TEL.getText());
-			// Логин в программе
-			if (EMP_LOGIN.getSelectionModel().getSelectedItem() != null) {
-				callStmt.setLong(9, EMP_LOGIN.getSelectionModel().getSelectedItem().getIUSRID());
-			} else {
-				callStmt.setNull(9, java.sql.Types.INTEGER);
-			}
-			/* Дата приема на работу */
-			callStmt.setDate(10,
-					(EMP_WORKSTART.getValue() != null) ? java.sql.Date.valueOf(EMP_WORKSTART.getValue()) : null);
-			/* Дата увольнения */
-			callStmt.setDate(11,
-					(EMP_WORKEND.getValue() != null) ? java.sql.Date.valueOf(EMP_WORKEND.getValue()) : null);
-			// выполнение
-			callStmt.execute();
-			// закрыть
-			callStmt.close();
+		
 		} catch (Exception e) {
 			DbUtil.Log_Error(e);
 		}
@@ -145,63 +97,7 @@ public class EditPmDocC {
 	@FXML
 	void Ok(ActionEvent event) {
 		try {
-			if (validate(EMP_EMAIL.getText()) == false) {
-				TrayNotification tray = new TrayNotification("Ошибка", "Ошибка в поле Email!", Notifications.ERROR);
-				tray.showAndWait();
-				Platform.runLater(() -> {
-					EMP_EMAIL.requestFocus();
-				});
-				return;
-			}
-
-			CallableStatement callStmt = conn.prepareCall("{ call PM_EMP_PKG.EDIT(?,?,?,?,?,?,?,?,?,?,?)}");
-			// Ошибка
-			callStmt.registerOutParameter(1, Types.VARCHAR);
-			// ИД сотрудника
-			if (pm_emp != null) {
-				callStmt.setLong(2, pm_emp.getEMP_ID());
-			} else {
-				callStmt.setNull(2, java.sql.Types.INTEGER);
-			}
-			/* Фамилия */
-			callStmt.setString(3, EMP_LASTNAME.getText());
-			/* Имя */
-			callStmt.setString(4, EMP_FIRSTNAME.getText());
-			/* Отчество */
-			callStmt.setString(5, EMP_MIDDLENAME.getText());
-			/* Должность */
-			callStmt.setString(6, EMP_POSITION.getText());
-			/* Email */
-			callStmt.setString(7, EMP_EMAIL.getText());
-			/* Телефон */
-			callStmt.setString(8, EMP_TEL.getText());
-			// Логин в программе
-			if (EMP_LOGIN.getSelectionModel().getSelectedItem() != null) {
-				callStmt.setLong(9, EMP_LOGIN.getSelectionModel().getSelectedItem().getIUSRID());
-			} else {
-				callStmt.setNull(9, java.sql.Types.INTEGER);
-			}
-			/* Дата приема на работу */
-			callStmt.setDate(10,
-					(EMP_WORKSTART.getValue() != null) ? java.sql.Date.valueOf(EMP_WORKSTART.getValue()) : null);
-			/* Дата увольнения */
-			callStmt.setDate(11,
-					(EMP_WORKEND.getValue() != null) ? java.sql.Date.valueOf(EMP_WORKEND.getValue()) : null);
-			// выполнение
-			callStmt.execute();
-			// выполнение
-			callStmt.execute();
-			if (callStmt.getString(1) == null) {
-				conn.commit();
-				setStatus(true);
-				callStmt.close();
-				OnClose();
-			} else {
-				conn.rollback();
-				setStatus(false);
-				Msg.Message(callStmt.getString(1));
-				callStmt.close();
-			}
+			
 		} catch (Exception e) {
 			DbUtil.Log_Error(e);
 		}
@@ -240,60 +136,10 @@ public class EditPmDocC {
 			new ConvConst().FormatDatePiker(EMP_WORKSTART);
 			new ConvConst().FormatDatePiker(EMP_WORKEND);
 
-			EMP_LASTNAME.setText(pm_emp.getEMP_LASTNAME());
-			EMP_FIRSTNAME.setText(pm_emp.getEMP_FIRSTNAME());
-			EMP_MIDDLENAME.setText(pm_emp.getEMP_MIDDLENAME());
-			EMP_POSITION.setText(pm_emp.getEMP_POSITION());
-			EMP_EMAIL.setText(pm_emp.getEMP_EMAIL());
-			EMP_TEL.setText(pm_emp.getEMP_TEL());
-			EMP_WORKSTART.setValue(pm_emp.getEMP_WORKSTART());
-			EMP_WORKEND.setValue(pm_emp.getEMP_WORKEND());
-			// -------------------
-			{
-				PreparedStatement sqlStatement = DbUtil.conn
-						.prepareStatement("select * from usr where usr.dusrfire is null");
-				ResultSet rs = sqlStatement.executeQuery();
-				ObservableList<USR> areas = FXCollections.observableArrayList();
-				while (rs.next()) {
-					USR list = new USR();
-					list.setIUSRID(rs.getLong("IUSRID"));
-					list.setCUSRLOGNAME(rs.getString("CUSRLOGNAME"));
-					list.setCUSRNAME(rs.getString("CUSRNAME"));
-					list.setCUSRPOSITION(rs.getString("CUSRPOSITION"));
-					areas.add(list);
-				}
-
-				sqlStatement.close();
-				rs.close();
-
-				EMP_LOGIN.setItems(areas);
-
-				FxUtilTest.getComboBoxValue(EMP_LOGIN);
-				FxUtilTest.autoCompleteComboBoxPlus(EMP_LOGIN,
-						(typedText,
-								itemToCompare) -> (itemToCompare.getCUSRLOGNAME() + "=" + itemToCompare.getCUSRNAME())
-										.toLowerCase().contains(typedText.toLowerCase()));
-
-				convertComboDisplayList();
-
-				for (USR usr : EMP_LOGIN.getItems()) {
-					if (usr.getIUSRID().equals(pm_emp.getEMP_LOGIN_L())) {
-						EMP_LOGIN.getSelectionModel().select(usr);
-						break;
-					}
-				}
-			}
+			
 		} catch (Exception e) {
 			DbUtil.Log_Error(e);
 		}
-	}
-
-	private VPM_EMP pm_emp;
-	Connection conn;
-
-	public void SetClass(VPM_EMP pm_emp, Connection conn) {
-		this.pm_emp = pm_emp;
-		this.conn = conn;
 	}
 
 }
